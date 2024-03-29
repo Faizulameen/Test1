1c5d33859cd4af64eeb401828700152e5f2f6ae7
Faizulameen <91254327+Faizulameen@users.noreply.github.com>
2024-03-29 15:00:22 +0530

    Based on keys it should come values

--- hash_manipulator.rb
+++ hash_manipulator.rb
@@ -1,73 +1,57 @@
+require 'test-unit'
 require 'json'
 
 class HashManipulator
-  attr_reader :data, :other_args
-
-  def initialize(data, *other_args)
+  def initialize(data, keys, other_args = [])
     @data = data
+    @keys = keys
     @other_args = other_args
   end
 
-  def problem_one_solution
-    clients = {}
-    data.each do |entry|
-      client = entry['client']
-      location = entry['location']
-      amount = entry['amount']
-
-      clients[client] ||= {}
-      clients[client][location] ||= { 'amount' => 0 }
-      clients[client][location]['amount'] += amount
-    end
-    clients.map { |client, locations| { 'client' => client, **locations } }
+ def problem_one
+    grouped_data = @data.group_by { |d| d[@keys[0]] }
+    grouped_data.transform_values do |client_data|
+      client_data.group_by { |d| d[@keys[1]] }.transform_values do |location_data|
+        location_data.group_by { |d| d[@keys[2]] }.transform_values do |month_data|
+          { 'amount' => month_data.map { |d| d[@keys[3]] }.compact.sum, 'amount2' => month_data.map { |d| d[@keys[4]] }.compact.sum }
+        end.reject { |k, v| v.nil? || v.empty? } # Remove nil or empty values
+      end.reject { |k, v| v.nil? || v.empty? } # Remove nil or empty values
+    end.reject { |k, v| v.nil? || v.empty? } # Remove nil or empty values
   end
 
-  def problem_two_solution
-    clients = {}
-    data.each do |entry|
-      client = entry['client']
-      location = entry['location']
-      month = entry['month']
-      amount = entry['amount']
-
-      clients[client] ||= {}
-      clients[client][location] ||= {}
-      clients[client][location][month] = amount
+  def problem_two
+    grouped_data = @data.group_by { |d| d[@keys[0]] }
+    grouped_data.transform_values do |client_data|
+      client_data.group_by { |d| d[@keys[1]] }.transform_values do |location_data|
+        location_data.group_by { |d| d[@keys[2]] }.transform_values do |month_data|
+          { 'amount' => month_data.map { |d| d[@keys[3]] }.compact.sum, 'amount2' => month_data.map { |d| d[@keys[4]] }.compact.sum }
+        end
+      end
     end
-    clients.map { |client, locations| { 'client' => client, **locations } }
   end
 
-  def problem_three_solution
-    clients = {}
-    data.each do |entry|
-      client = entry['client']
-      location = entry['location']
-      month = entry['month']
-      amount = entry['amount']
-      amount2 = entry['amount2']
-
-      clients[client] ||= {}
-      clients[client][location] ||= {}
-      clients[client][location][month] ||= {}
-      clients[client][location][month]['amount'] = amount
-      clients[client][location][month]['amount2'] = amount2.nil? ? 'null' : amount2
+  def problem_three
+    grouped_data = @data.group_by { |d| d[@keys[0]] }
+    grouped_data.transform_values do |client_data|
+      client_data.group_by { |d| d[@keys[1]] }.transform_values do |location_data|
+        location_data.group_by { |d| d[@keys[2]] }.transform_values do |month_data|
+          { 'amount' => month_data.map { |d| d[@keys[3]] }.compact.sum, 'amount2' => month_data.map { |d| d[@keys[4]] }.compact.sum }
+        end
+      end
     end
-    clients.map { |client, locations| { 'client' => client, **locations } }
   end
 end
 
 data = JSON.parse(File.read('./data.json'))
-# Running assertions to test the solutions
-assert_equal data.dig('problem_one', 'response'), HashManipulator.new(data.dig('problem_one', 'data')).problem_one_solution
 
-assert_equal data.dig('problem_two', 'response'), HashManipulator.new(data.dig('problem_two', 'data')).problem_two_solution
-
-assert_equal data.dig('problem_three', 'response'), HashManipulator.new(data.dig('problem_three', 'data')).problem_three_solution
+assert_equal data.dig('problem_one', 'response'), HashManipulator.new(data.dig('problem_one', 'data'), %w[client location amount]).problem_one
+assert_equal data.dig('problem_two', 'response'), HashManipulator.new(data.dig('problem_two', 'data'), %w[client location month amount amount2]).problem_two
+assert_equal data.dig('problem_three', 'response'), HashManipulator.new(data.dig('problem_three', 'data'), %w[client location month amount amount2]).problem_three
 
 def assert_equal(expected, actual)
    if expected.to_s == actual.to_s  
-     puts "Assertion passed"    
+     puts "Assertion passed #{expectd} equal to #{actual}"    
    else    
-     puts "Assertion failed"    
+     puts "Assertion failed #{expectd} not equal to #{actual}"    
    end    
- end 
+ end
